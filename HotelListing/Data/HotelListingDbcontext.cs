7db41c18c6b20b5874fae05bce3106a965aebca6
MouadAmzil <mouadmouad2026@gmail.com>
2022-09-09 06:51:42 +0100

    Add migration To data base

--- HotelListing/Data/HotelListingDbcontext.cs
+++ HotelListing/Data/HotelListingDbcontext.cs
@@ -6,7 +6,10 @@ namespace HotelListing.Data
     {
         public HotelListingDbcontext(DbContextOptions option) : base(option)
         {
-        } 
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
 
     }
 }
